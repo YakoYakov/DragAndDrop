d616a6ce3c84c023bc14f227da8e2b27fa61bb7a
YakoYakov <yako.p.yakov@gmail.com>
2021-01-27 18:34:34 +0200

    Created ProjectInput class, that will hold main logic of rendering html

--- DragAndDropApp/src/app.ts
+++ DragAndDropApp/src/app.ts
@@ -1,1 +1,25 @@
-// Code goes here!
+class ProjectInput {
+  templateElement: HTMLTemplateElement;
+  hostElement: HTMLDivElement;
+
+  constructor() {
+    this.templateElement = document.getElementById(
+      'project-input'
+    )! as HTMLTemplateElement;
+
+    this.hostElement = document.getElementById('app')! as HTMLDivElement;
+
+    const elementToAttach = document.importNode(
+      this.templateElement.content,
+      true
+    ).firstElementChild as HTMLFormElement;
+
+    this.attach(elementToAttach);
+  }
+
+  private attach(elementToAttach: HTMLFormElement) {
+    this.hostElement.insertAdjacentElement('afterbegin', elementToAttach);
+  }
+}
+
+const prjInput = new ProjectInput();
