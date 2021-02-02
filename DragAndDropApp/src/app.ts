fb45ebd202dcd44afc8bb8444a74152c51bb180b
YakoYakov <yako.p.yakov@gmail.com>
2021-02-02 11:43:23 +0200

    Added ProjectList class for rendering projects

--- DragAndDropApp/src/app.ts
+++ DragAndDropApp/src/app.ts
@@ -60,6 +60,42 @@ function Bind(_: any, _2: string | symbol, descriptor: PropertyDescriptor) {
   return fixedBindingMethod;
 }
 
+// ProjectList class
+
+class ProjectList {
+    templateElement: HTMLTemplateElement;
+    hostElement: HTMLDivElement;
+    elementToAttach: HTMLElement;
+
+
+    constructor(private type: 'active' | 'finished') {
+        this.templateElement = document.getElementById(
+            'project-list'
+        )! as HTMLTemplateElement;
+
+        this.hostElement = document.getElementById('app')! as HTMLDivElement;
+
+        this.elementToAttach = document.importNode(
+            this.templateElement.content,
+            true
+        ).firstElementChild as HTMLElement;
+
+        this.elementToAttach.id = `${this.type}-projects`;
+        this.attach();
+        this.renderContent();
+    }
+
+    private renderContent() {
+        const listId = `${this.type}-projects-list`;
+        this.elementToAttach.querySelector('ul')!.id = listId;
+        this.elementToAttach.querySelector('h2')!.textContent = this.type.toUpperCase() + ' PROJECTS';
+    }
+
+    private attach() {
+        this.hostElement.insertAdjacentElement('beforeend', this.elementToAttach);
+    }
+}
+
 // Project input class
 
 class ProjectInput {
@@ -156,3 +192,5 @@ class ProjectInput {
 }
 
 const prjInput = new ProjectInput();
+const activePrjList = new ProjectList('active');
+const finishedPrjList = new ProjectList('finished');
